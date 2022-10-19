61fafd1e96de9aa1c8d7e30dca4c546a25997a3d
Igor Tozetti <igorct77@gmail.com>
2022-10-19 13:58:32 -0300

    debounce.js

--- assets/js/modules/scroll-animation.js
+++ assets/js/modules/scroll-animation.js
@@ -1,12 +1,15 @@
+import debounce from "./debounce.js";
+
 export default class ScrollAnimation {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.halfWindow = window.innerHeight * 0.7;
 
     // bind
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
+  // pega distancia de cada item em relação ao topo do site
   getDistance() {
     this.distance = [...this.sections].map((section) => {
       const offset = section.offsetTop;
@@ -19,6 +22,7 @@ export default class ScrollAnimation {
 
   // verifica a distancia em cada objeto em relação ao scroll do site
   checkDistance() {
+    console.log("ativou");
     this.distance.forEach((section) => {
       if (window.pageYOffset > section.offset) {
         section.element.classList.add("active");
