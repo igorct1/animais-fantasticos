3fef414916d19e65c02befa2b2a9dce58cf2197f
Igor Tozetti <igorct77@gmail.com>
2022-10-19 13:37:50 -0300

    new scroll-animation

--- assets/js/modules/scroll-animation.js
+++ assets/js/modules/scroll-animation.js
@@ -4,23 +4,36 @@ export default class ScrollAnimation {
     this.halfWindow = window.innerHeight * 0.7;
 
     // bind
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const sectionVisible = sectionTop - this.halfWindow < 0;
-      if (sectionVisible) {
-        section.classList.add("active");
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfWindow),
+      };
+    });
+  }
+
+  // verifica a distancia em cada objeto em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((section) => {
+      if (window.pageYOffset > section.offset) {
+        section.element.classList.add("active");
+      } else if (section.element.classList.contains("active")) {
+        section.element.classList.remove("active");
       }
     });
   }
 
   init() {
     if (this.sections.length) {
-      this.animaScroll();
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
     }
-    window.addEventListener("scroll", this.animaScroll);
+    return this;
   }
 }
