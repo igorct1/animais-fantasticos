7da435a1dbcdfd45443d6949bb3238388d051954
Igor Tozetti <igorct77@gmail.com>
2022-10-19 13:10:22 -0300

    refactoring scroll-animation

--- assets/js/modules/scroll-animation.js
+++ assets/js/modules/scroll-animation.js
@@ -1,18 +1,26 @@
-export default function initScrollAnimation() {
-  const sections = document.querySelectorAll("[data-anime='scroll']");
-  const halfWindow = window.innerHeight * 0.7;
-  function animaScroll() {
-    sections.forEach((section) => {
+export default class ScrollAnimation {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.halfWindow = window.innerHeight * 0.7;
+
+    // bind
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const sectionVisible = sectionTop - halfWindow < 0;
+      const sectionVisible = sectionTop - this.halfWindow < 0;
       if (sectionVisible) {
         section.classList.add("active");
       }
     });
   }
-  if (sections.length) {
-    sections[0].classList.add("active");
-    // animaScroll();
-    window.addEventListener("scroll", animaScroll);
+
+  init() {
+    if (this.sections.length) {
+      this.animaScroll();
+    }
+    window.addEventListener("scroll", this.animaScroll);
   }
 }
